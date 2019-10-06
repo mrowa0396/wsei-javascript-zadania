0cc8b969da547ed2b083e6f64bf956abf15a174d
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2019-10-06 17:34:10 +0200

    Add files via upload

---
+++ 1_Zadania/obowiązkowe/2_Tablice/zad4.js
@@ -0,0 +1,17 @@
+function multiply(array){
+
+    var suma = 1;
+
+
+    for (i=0; array < array.length; i++){
+        suma = suma * array[i];
+    
+        
+    
+    }
+    return suma;
+
+
+
+}
+multiply([1,2,3,4,5,6,7]);

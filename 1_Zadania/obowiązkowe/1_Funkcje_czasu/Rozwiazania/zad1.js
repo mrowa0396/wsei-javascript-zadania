75b7a6a6ba011f7fff7b67066748bb259f1e18d3
Mateusz Sowiński <msowinski@sevillemore.com>
2019-10-06 08:59:18 +0200

    Zadanie

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/Rozwiazania/zad1.js
@@ -0,0 +1,21 @@
+//zadanie 1
+
+function countHello(number) {
+
+    var counter = 1;
+    var interval = setInterval(() => {
+
+        console.log('Hello', counter++)
+        if (counter > number){
+            clearInterval(interval)
+        }
+    }, 500)
+
+}
+
+countHello(5);
+
+
+
+
+

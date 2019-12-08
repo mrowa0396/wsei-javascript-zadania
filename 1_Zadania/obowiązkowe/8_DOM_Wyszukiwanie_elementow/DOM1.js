d86f7afe9a27a29f857c51decdbeb5c8a0292591
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2019-12-08 16:39:56 +0100

    Add files via upload

---
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/DOM1.js
@@ -0,0 +1,33 @@
+const tiletle = document.querySelector('.title');
+
+function getDataAnimation(element) {
+
+let dataAnimation = element.getAttribute('data-animation');
+return dataAnimation;
+
+
+};
+
+
+
+const test = getDataAnimation(title);
+
+console.log(test);
+
+const home = document.getElementById('home');
+const home2 = document.querySelector('#home');
+
+const li1 = document.querySelector('li[data-direction]');
+
+const block = document.querySelector('.block');
+
+const first = document.querySelector('nav li');
+const second = document.querySelector('div p');
+const third = document.querySelector('article div');
+
+var articcle = document.querySelector('article.first');
+console.log(article.querySelectoryAll('h1').length);
+
+
+
+

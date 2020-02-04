1803213484d2e0ac8e3bbc88982316c1f1647647
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2020-02-04 21:57:09 +0100

    Update zadanie05.js

---
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie05.js
@@ -0,0 +1,18 @@
+//Zad5
+document.addEventListener('DOMContentLoaded', function() {
+
+    var buttons = document.querySelectorAll(".moveBtn");
+    var list1 = document.querySelector("#list1");
+    var list2 = document.querySelector("#list2");
+    
+    for (var i = 0; i < buttons.length; i++) {
+    
+        buttons[i].addEventListener("click", function() {
+            if (this.parentElement.parentElement.id == "list1") {
+                list2.appendChild(this.parentElement);
+            } else {
+                list1.appendChild(this.parentElement);
+            }
+        });
+    }   
+});

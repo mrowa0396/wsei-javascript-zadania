0eee1d93065c6b65a6411f2df205d67e0411e7af
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2020-02-04 21:16:54 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/app.js
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/app.js
@@ -1,32 +1,33 @@
-/**
+**
  * Zagniezdzanie funkcji.
  * Przesledz krok po kroku kazda linijke kodu. Kazda!
  * Napisz komentarze dla kazdej z linijek kodu
  */
 
 
-//Twoj komentarz ...
+// tworze funkcje o nazwie jeden 
 function jeden() {
 
-    //Twoj komentarz ...
+    // tworze zmienną o wartosci 1 w local scopie, czyli zmienna nie moze wyjsc poza funkcje jeden
     var zmienna1 = 1;
 
-    //Twoj komentarz ...
+    // tworze zmienna dwa, jest skladowa funkci jeden wiec  moge w niej skorzystac z funkcji zmienna1
     function dwa() {
 
-        //Twoj komentarz ...
+        // wypisuje w konsoli wartosc zmiennej1
         console.log(zmienna1);
 
-        //Twoj komentarz ...
+        // tworze zmienna 2 o wartosci 3 w local scopie dla funkcji dwa
         var zmienna2 = 3;
     }
 
-    //Twoj komentarz ...
+    // wywoluje funkcje dwa
     dwa();
 
-    //Twoj komentarz ...
+    // VM338:21 Uncaught ReferenceError: zmienna2 is not defined - wywala error poniewaz zmienna2 jest w local scopie dla funkcji dwa. 
+// gdyby byla to zmienna w global scopie otrzymalisbysmy wynik.
     console.log(zmienna2)
 }
 
-//Twoj komentarz ...
-jeden()
+// wywołanie funkcji jeden
+    jeden();

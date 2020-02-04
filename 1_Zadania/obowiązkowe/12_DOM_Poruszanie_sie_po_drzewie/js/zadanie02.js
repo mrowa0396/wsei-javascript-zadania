d0ea484d36da5f08f287771997e6cfe0b3f0131c
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2020-02-04 21:52:08 +0100

    Update zadanie02.js

---
+++ 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie02.js
@@ -0,0 +1,12 @@
+// Zad2
+
+var buttons = document.getElementsByClassName("button");
+
+function clickListener1() {
+  var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  this.parentElement.style.backgroundColor = randomColor;
+}
+
+for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", clickListener1);
+}

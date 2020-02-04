2b188cca3c6e656cf974aae04811adf3d2c99508
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2020-02-04 21:55:01 +0100

    Update zadanie02.js

---
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie02.js
@@ -0,0 +1,29 @@
+// Zad 2 
+  
+document.addEventListener('DOMContentLoaded', function() {
+
+    var button = document.querySelector(".button");
+    var orderId = document.querySelector("#orderId");
+    var item = document.querySelector("#item");
+    var quantity = document.querySelector("#quantity");
+    
+    button.addEventListener( "click", function() {
+      var newTr = document.createElement("tr");
+
+      var tbody = document.querySelector("tbody");
+      tbody.appendChild(newTr);
+
+      var newTd = document.createElement("td");
+      newTr.appendChild(newTd);
+    
+      var newTd2 = document.createElement("td");
+      newTr.appendChild(newTd2);
+    
+      var newTd3 = document.createElement("td");
+      newTr.appendChild(newTd3);
+    
+      newTd.innerHTML = orderId.value;
+      newTd2.innerHTML = item.value;
+      newTd3.innerHTML = quantity.value;
+    });    
+});

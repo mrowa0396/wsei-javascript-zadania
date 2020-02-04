458c530c63b16aa6626464edfda59fc714405487
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2020-02-04 21:33:17 +0100

    Update zadanie00.js

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -0,0 +1,26 @@
+// Zad 0a
+
+let parents = document.querySelectorAll('.parent');
+
+for(let i = 0; i < parents.length; i++) {
+    parents[i].addEventListener('mouseover', function() {
+        this.children[0].style.display = 'block';
+    }) 
+    parents[i].addEventListener('mouseleave', function() {
+        this.children[0].style.display = 'none';
+    }) 
+}
+
+// 0b
+
+for (var j = 0; j < parents.length; j++) {
+    parents[j].addEventListener("mouseout", function() {
+        var parent = this;
+
+        function hideChildrenElement(parent){
+            parent.querySelector('.children').style.display = 'none';
+        }
+
+        hideChildrenElement(parent);
+    });
+}
